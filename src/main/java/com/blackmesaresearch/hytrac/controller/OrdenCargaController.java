34af59bfb0e56ec05532083217a1dfc031a3e3e3
dmelhado <melhado.dante@gmail.com>
2026-05-11 23:24:17 -0300

    fix - posible fix api?

--- src/main/java/com/blackmesaresearch/hytrac/controller/OrdenCargaController.java
+++ src/main/java/com/blackmesaresearch/hytrac/controller/OrdenCargaController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 
 @RestController
-@RequestMapping("/ordenes")
+@RequestMapping("/api/ordenes")
 @CrossOrigin("*")
 public class OrdenCargaController {
 
