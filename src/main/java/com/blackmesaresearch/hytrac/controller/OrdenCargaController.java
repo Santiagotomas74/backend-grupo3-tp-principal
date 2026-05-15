9bc17308f50522d94db1115518e7b313b8bed780
Luis <luisvelizestudiante916@gmail.com>
2026-05-14 22:49:35 -0300

    feat: agregando endpoint para cancelar ordenes

--- src/main/java/com/blackmesaresearch/hytrac/controller/OrdenCargaController.java
+++ src/main/java/com/blackmesaresearch/hytrac/controller/OrdenCargaController.java
@@ -1,16 +1,22 @@
 package com.blackmesaresearch.hytrac.controller;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import com.blackmesaresearch.hytrac.model.core.OrdenCarga;
-import com.blackmesaresearch.hytrac.service.OrdenCargaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.blackmesaresearch.hytrac.dto.request.OrdenCargaRequestDTO;
 import com.blackmesaresearch.hytrac.dto.response.OrdenCargaDetalleResponseDTO;
 import com.blackmesaresearch.hytrac.dto.response.OrdenCargaResponseDTO;
-import org.springframework.http.ResponseEntity;
-import java.util.Map;
+import com.blackmesaresearch.hytrac.service.OrdenCargaService;
 
 
 @RestController
